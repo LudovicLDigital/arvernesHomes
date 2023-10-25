d32fc13abea34b135f1934137e30fa3bd2adc7a5
LudovicLDigital <ludovic.laurichesse@l-digital.net>
2023-10-25 16:27:42 +0200

    test for static image

--- hostingerImages.ts
+++ hostingerImages.ts
@@ -2,5 +2,5 @@ import {string} from "prop-types";
 
 // generate a function that returns the root path of the image src passed to the function
 export default function hostingerImages(src: string) {
-    return `/files/public_html/${src}`;
+    return `${src}`;
 }
