880ead3de12d052ca60d94479c0fb9b1239a19fc
LudovicLDigital <ludovic.laurichesse@l-digital.net>
2023-10-25 16:18:44 +0200

    test for static image

---
+++ hostingerImages.ts
@@ -0,0 +1,6 @@
+import {string} from "prop-types";
+
+// generate a function that returns the root path of the image src passed to the function
+export default function hostingerImages(src: string) {
+    return `/files/public_html/${src}`;
+}
--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
 
     // Optional: Change the output directory `out` -> `dist`
     distDir: 'build',
+    images: {
+        loader: 'custom',
+        loaderFile: './hostingerImages.ts',
+    },
 }
 
 module.exports = nextConfig
